906fde038f61fdf06bdd5c8281c84f5fa72aa9eb
ttoby <toby.r.allen@gmail.com>
2022-05-14 17:54:28 +0100

    Fix how providers are specified

--- versions.tf
+++ versions.tf
@@ -1,15 +1,19 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
 
     random = {
       source  = "hashicorp/random"
       version = "3.0.0"
     }
+    
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    
+    
+    
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.1.0"
 }
